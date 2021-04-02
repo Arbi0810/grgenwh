67d6e04384607b6a32e71a8a05a5a9c15a89e13f
Arbi0810 <tamilin2012@gmail.com>
2021-04-02 22:33:34 +0300

    1

---
+++ iJvpZaLqDJ38NFifBnw.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+
+        <title>Article</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body>
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container box_1620">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <a class="navbar-brand logo_h" href="index.php">cleanliness</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                            <ul class="nav navbar-nav menu_nav justify-content-center">
+                                <li class="nav-item "><a class="nav-link" href="index.php">Main</a></li>
+                                <li class="nav-item"><a class="nav-link" href="blog.php">Articles</a></li>
+                                <li class="nav-item"><a class="nav-link" href="contact.php">Contacts</a></li>
+                                <li class="nav-item"><a class="nav-link" href="terms.php">Terms and conditions</a></li>
+                                <li class="nav-item"><a class="nav-link" href="policy.php">Privacy policy</a></li>
+                                <li class="nav-item submenu dropdown">
+                                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Article</a>
+                                    <ul class="dropdown-menu">
+                                        
+                                        <li class="nav-item"><a class="nav-link" href="558cf1ebc145d6638a742cc76d62203a.php">The best cardio exercises you ...</a></li>
+                                        
+
+                                        <li class="nav-item"><a class="nav-link" href="866960b8e34d6a1271dd16e7c021efe6.php">Best home remedies for leg swe...</a></li>
+                                        
+
+                                        <li class="nav-item"><a class="nav-link" href="iJvpZaLqDJ38NFifBnw.php">What are the benefits of rice ...</a></li>
+                                        
+
+                                    </ul>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+
+        <!--================End Home Banner Area =================-->
+        
+        <!--================Blog Area =================-->
+        <section class="blog_area single-post-area p_120">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 posts-list">
+                        <div class="single-post row">
+                            <div class="col-lg-12">
+                                <div class="feature-img">
+                                    <img class="img-fluid" src="./assets/images/angelos-michalopoulos-t7j_yjuv8qi-unsplash.jpg" alt="">
+                                </div>									
+                            </div>
+
+                            <div class="col-lg-9 col-md-9 blog_details">
+                                <h2>What are the benefits of rice bran oil?</h2>
+                                <p class="excert">
+                                    <strong>What are the benefits of rice bran oil?</strong> Rice oil is a great choice for a healthy home-cooked meal. Food cooked in this oil has a characteristic taste and aroma. It is obtained by cold-pressing rice husks, in which actually contains all the nutrients. It is used for cooking dishes, added to fresh salads and even desserts.Most often, these are drinks made from cereals, but there are also a lot of recipes for adding fish, pork, chicken, and even vegetables to your menu. In addition, the typical rice oil is suitable for frying over medium heat.Rice bran oil has a lot of useful properties and contains, in particular, a lot of vitamin E, fatty acids and squalene, while it is less fatty and calorific than other vegetable oils.Rice bran oil it is recognized as an essential oil for both internal consumption and cooking. Due to its high content of vitamins and minerals, it is the preferred oil for both cooking and nursing. It is also suitable for use in different types of soups and for baking.Soy milk.Soy milk has a pleasant nutty taste with a delicate aroma. It is obtained by cold-pressing rice husks, in which actually contain all the nutrients. It is a good alternative to other soy drinks that are more bitter and astringent.Fermented milk products.They are another good option for those that want to add nutty flavors to their dishes. They are excellent at adding protein, vitamins, and minerals to vegetables and meat.Nuts.A handful of nuts is a great gift for any holiday. Especially useful are cashews, almonds, and walnuts, which are simply irresistible. What a relief it is to eat a handful of nuts in the morning, instead of the usual yogurt and before Breakfast. This simple yet very effective drink reduces the concentration of cholesterol, improves digestion and metabolism. Water and nuts - what is fat?Carrot juice for strengthening the heart and constipation.Carrots.Carrots are delicious, but very high-calorie. They contain a lot of fiber and vitamins, but they are essentially unimportant for weight maintenance and weight loss. This the product of the inner ear. The main value of carrots is that it contains amino acids, which are very important for the body. Otherwise, the consumption of carrots is not appropriate. for health, as they are very high in calories.This is also true for young carrots that are at least a week old. These are the "power of early childhood."4 types of protein powder for diabetics.Coconut oil.Coconut oil is one of the best vegetable oils for the heart and blood vessels. It contains a lot of vitamin E, which helps improve blood vessels, and it reduces the level of triglycerides, cholesterol, and blood sugar. This article is for informational purposes only
+                                </p>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="blog_right_sidebar">
+                            <aside class="single_sidebar_widget search_widget">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" >
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" type="button"><i class="lnr lnr-magnifier"></i></button>
+                                    </span>
+                                </div><!-- /input-group -->
+                                <div class="br"></div>
+                            </aside>
+
+                            <aside class="single_sidebar_widget popular_post_widget">
+                                <h3 class="widget_title">Popular</h3>
+                                
+                                <div class="media post_item">
+                                    <img style="width: 50%" src="./assets/images/anna-pelzer-igfigp5onv0-unsplash.jpg" alt="post">
+                                    <div class="media-body">
+                                        <a href="558cf1ebc145d6638a742cc76d62203a.php"><h3>The best cardio exercises you can do at home</h3></a>
+                                        <p>30.03.2021</p>
+                                    </div>
+                                </div>
+                                
+
+                                <div class="media post_item">
+                                    <img style="width: 50%" src="./assets/images/isra-e-shjmuyqsrn8-unsplash.jpg" alt="post">
+                                    <div class="media-body">
+                                        <a href="866960b8e34d6a1271dd16e7c021efe6.php"><h3>Best home remedies for leg swelling</h3></a>
+                                        <p>30.03.2021</p>
+                                    </div>
+                                </div>
+                                
+
+                                <div class="media post_item">
+                                    <img style="width: 50%" src="./assets/images/angelos-michalopoulos-t7j_yjuv8qi-unsplash.jpg" alt="post">
+                                    <div class="media-body">
+                                        <a href="iJvpZaLqDJ38NFifBnw.php"><h3>What are the benefits of rice bran oil?</h3></a>
+                                        <p>30.03.2021</p>
+                                    </div>
+                                </div>
+                                
+
+                                <div class="br"></div>
+                            </aside>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================Blog Area =================-->
+        
+        <!--================ start footer Area  =================-->
+        <footer class="footer-area">
+
+            <div class="border_line"></div>
+            <div class="container">
+                <div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+                    </p>
+
+                </div>
+            </div>
+        </footer>
+        <!--================ End footer Area  =================-->
+
+        <div class='cookie-banner'>
+            <p>
+                The site uses cookies. They allow us to recognize you and get information about your user experience.By continuing to browse the site, I agree to the use of cookies by the site owner in accordance with  <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie">Cookie policy</a>
+            </p>
+            <button class='close-cookie'>&times;</button>
+        </div>
+		<!--================ End footer Area  =================-->
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        <script src="js/theme.js"></script>
+        <script>
+            window.onload = function() {
+                $('.close-cookie').click(function () {
+                    $('.cookie-banner').fadeOut();
+                })
+            }
+        </script>
+        <script>
+            let elems = document.querySelectorAll('.server-name');
+            elems.forEach((elem) => {
+                elem.innerHTML = window.location.hostname
+            })
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,239 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+
+		<title>attractiveness</title>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+        <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
+        <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
+        <link rel="stylesheet" href="vendors/animate-css/animate.css">
+        <link rel="stylesheet" href="vendors/swiper/css/swiper.min.css">
+        <!-- main css -->
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+    </head>
+    <body>
+        
+        <!--================Header Menu Area =================-->
+        <header class="header_area">
+            <div class="main_menu">
+            	<nav class="navbar navbar-expand-lg navbar-light">
+					<div class="container box_1620">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<a class="navbar-brand logo_h" href="index.php">sophistication</a>
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+							<ul class="nav navbar-nav menu_nav justify-content-center">
+								<li class="nav-item active"><a class="nav-link" href="index.php">Main</a></li>
+								<li class="nav-item"><a class="nav-link" href="blog.php">Articles</a></li>
+								<li class="nav-item"><a class="nav-link" href="contact.php">Contacts</a></li>
+								<li class="nav-item"><a class="nav-link" href="terms.php">Terms and conditions</a></li>
+								<li class="nav-item"><a class="nav-link" href="policy.php">Privacy policy</a></li>
+								<li class="nav-item submenu dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Article</a>
+									<ul class="dropdown-menu">
+										
+										<li class="nav-item"><a class="nav-link" href="558cf1ebc145d6638a742cc76d62203a.php">The best cardio exercises you ...</a></li>
+										
+
+										<li class="nav-item"><a class="nav-link" href="866960b8e34d6a1271dd16e7c021efe6.php">Best home remedies for leg swe...</a></li>
+										
+
+										<li class="nav-item"><a class="nav-link" href="iJvpZaLqDJ38NFifBnw.php">What are the benefits of rice ...</a></li>
+										
+
+									</ul>
+								</li> 
+
+							</ul>
+
+						</div> 
+					</div>
+            	</nav>
+            </div>
+        </header>
+        <!--================Header Menu Area =================-->
+        
+        <!--================Home Banner Area =================-->
+
+        <!--================End Mission Area =================-->
+        
+        <!--================Success Area =================-->
+
+        <!--================End Success Area =================-->
+        
+        <!--================Project Area =================-->
+        <section class="project_area">
+        	<div class="row m0">
+				
+        		<div class="col-lg-4 col-md-6 p0">
+
+					<div class="project_item">
+        				<img src="./assets/images/anna-pelzer-igfigp5onv0-unsplash.jpg" alt="">
+
+						<a href="558cf1ebc145d6638a742cc76d62203a.php"><h4>The best cardio exercises you can do at home</h4></a>
+
+
+
+        			</div>
+
+        		</div>
+				
+
+        		<div class="col-lg-4 col-md-6 p0">
+
+					<div class="project_item">
+        				<img src="./assets/images/isra-e-shjmuyqsrn8-unsplash.jpg" alt="">
+
+						<a href="866960b8e34d6a1271dd16e7c021efe6.php"><h4>Best home remedies for leg swelling</h4></a>
+
+
+
+        			</div>
+
+        		</div>
+				
+
+        		<div class="col-lg-4 col-md-6 p0">
+
+					<div class="project_item">
+        				<img src="./assets/images/angelos-michalopoulos-t7j_yjuv8qi-unsplash.jpg" alt="">
+
+						<a href="iJvpZaLqDJ38NFifBnw.php"><h4>What are the benefits of rice bran oil?</h4></a>
+
+
+
+        			</div>
+
+        		</div>
+				
+
+
+        	</div>
+        </section>
+		<section class="contact_area p_120">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-lg-3">
+						<div class="contact_info">
+							<div class="info_item">
+
+								<h6>6859 Connelly Forges Apt. 742
+Ebertshire, OH 37605</h6>
+
+							</div>
+							<div class="info_item">
+
+								<h6><a href="#">+4068360292698</a></h6>
+
+							</div>
+							<div class="info_item">
+
+								<h6><a href="#">giovani52@hotmail.com</a></h6>
+
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-9">
+						<form class="row contact_form" action="thanks.php" method="post" id="contactForm" novalidate="novalidate">
+							<div class="col-md-6">
+								<div class="form-group">
+									<input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+								</div>
+								<div class="form-group">
+									<input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
+								</div>
+								<div class="form-group">
+									<input type="text" class="form-control" id="subject" name="subject" placeholder="Phone number">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<textarea class="form-control" name="message" id="message" rows="1" ></textarea required>
+								</div>
+							</div>
+							<div class="col-md-12 text-right">
+								<label class="terms-field">
+									<input type="checkbox" name="terms" value="check" required="required"/>
+									<a href="policy.php" target="_blank">
+										I have read and agree to the terms of the user agreement
+									</a>
+								</label>
+								<button type="submit" value="submit" class="btn submit_btn">Send</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</section>
+        <footer class="footer-area">
+
+            <div class="border_line"></div>
+			<div class="container">
+				<div class="row footer-bottom d-flex justify-content-between align-items-center">
+                    <p class="col-lg-8 col-md-8 footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+</p>
+
+                </div>
+			</div>
+        </footer>
+		<!--================ End footer Area  =================-->
+
+		<div class='cookie-banner'>
+			<p>
+				The site uses cookies. They allow us to recognize you and get information about your user experience.By continuing to browse the site, I agree to the use of cookies by the site owner in accordance with  <a target="_blank" href="https://en.wikipedia.org/wiki/HTTP_cookie">Cookie policy</a>
+			</p>
+			<button class='close-cookie'>&times;</button>
+		</div>
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
+        <script src="vendors/isotope/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/counter-up/jquery.waypoints.min.js"></script>
+        <script src="vendors/counter-up/jquery.counterup.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="vendors/popup/jquery.magnific-popup.min.js"></script>
+        <script src="vendors/swiper/js/swiper.min.js"></script>
+        <script src="js/theme.js"></script>
+		<script>
+			window.onload = function() {
+				$('.close-cookie').click(function () {
+					$('.cookie-banner').fadeOut();
+				})
+			}
+		</script>
+		<script>
+			let elems = document.querySelectorAll('.server-name');
+			elems.forEach((elem) => {
+				elem.innerHTML = window.location.hostname
+			})
+		</script>
+    </body>
+</html>
